c5d074f64175f5e7a49849469548e2f70aec92ea
Sungjee Yoo <sungjee.u@gmail.com>
2026-07-13 01:28:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'showup-v2.02';
+const CACHE = 'showup-v2.03';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
